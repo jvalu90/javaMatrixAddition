19cf83a0eb849f6e79d04ffc87db09ef9a3e440d
Jimmy Valencia Urbano <jvalu90@gmail.com>
2022-08-09 19:51:13 -0500

    Variable initialization

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner inputs = new Scanner(System.in).useLocale(Locale.US);
+        int matrixSize;
+
+        System.out.println("This project is done with two square arrays of size n");
+        System.out.print("Please indicate the size of the matrix 'M': ");
+        matrixSize = inputs.nextInt();
+
+        //Insert try, catch and Finally
+        
     }
 }
